e50104242e0652218be754ec8470ffaf5a30541a
Soboro <junseo1020@gmail.com>
2016-04-10 03:29:53 +0900

    modified Google maps intent in school introduce layout

--- app/src/main/java/com/junseo/dphs/fragment/Page3.java
+++ app/src/main/java/com/junseo/dphs/fragment/Page3.java
@@ -3,6 +3,7 @@ package com.junseo.dphs.fragment;
 /**
  * Created by Junseo on 16. 3. 18..
  */
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -13,12 +14,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.junseo.dphs.R;
 
 @SuppressLint("ValidFragment")
 public class Page3 extends Fragment {
+
     Context mContext;
 
     public Page3(Context context) {
@@ -29,6 +34,20 @@ public class Page3 extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.school_info_3, null);
+
+        view.findViewById(R.id.mapCard).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=대평고등학교");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
 
         return view;
     }
--- app/src/main/java/com/junseo/dphs/info/School_Info.java
+++ app/src/main/java/com/junseo/dphs/info/School_Info.java
@@ -20,7 +20,10 @@ import it.neokree.materialtabs.MaterialTabListener;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.junseo.dphs.R;
 
 
